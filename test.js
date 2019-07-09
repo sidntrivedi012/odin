4020a34826d2bb83d576e834ddf228238493f858
sidntrivedi012 <sidntrivedi012@gmail.com>
2019-07-09 21:47:58 +0530

    file creation

--- test.js
+++ test.js
@@ -4,23 +4,35 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_API_KEY
 });
 var fs = require("fs");
-var res = {};
-
+var obj = [
+  {
+    contributor: "name",
+    commits: 0
+  }
+];
+var f = 0;
 function getLeaderboard() {
   octokit.repos.listForOrg({ org: "osdc" }).then(repos => {
     repos.data.forEach(({ name }) => {
       octokit.repos.listCommits({ owner: "osdc", repo: name }).then(commits => {
         commits.data.forEach(commit => {
-          console.log(commit.commit.author.name);
+          var osdcian = commit.commit.author.name;
+          var osdcian_data = {
+            contributor: commit.commit.author.name,
+            commits: 1
+          };
+          let temp = obj.find(e => e.contributor === osdcian);
+          if (temp != undefined) {
+            temp.commits += 1;
+          } else {
+            obj.push(osdcian_data);
+          }
+          fs.writeFile("final.json", JSON.stringify(obj), function(err) {
+            if (err) throw err;
+          });
         });
       });
     });
   });
 }
 getLeaderboard();
-// fs.readFile("./commits.json", "utf-8", (err, data) => {
-//   var res = JSON.parse(data);
-//   res.data.forEach(commit => {
-//     console.log(commit.commit.author.name);
-//   });
-// });
