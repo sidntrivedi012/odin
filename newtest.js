1dfc1b61c9f54fd836a08a9b1d0d67201a5e21cc
sidntrivedi012 <sidntrivedi012@gmail.com>
2019-07-09 15:48:33 +0530

    getting authors

--- newtest.js
+++ newtest.js
@@ -1,19 +1,16 @@
 const Octokit = require("@octokit/rest");
 const octokit = new Octokit();
-var fs = require("fs");
-var repos = [];
-var res = {};
 
 function getLeaderboard() {
   octokit.repos.listForOrg({ org: "osdc" }).then(repos => {
-    repos.data.forEach(({ name }) => {
+    repos.forEach(({ name }) => {
       octokit.repos.listCommits({ owner: "osdc", repo: name }).then(commits => {
-        commits.data.forEach(commit => {
-          const { committer, author } = commit;
-          console.log(author);
+        commits.forEach(commit => {
+          console.log(commit);
         });
       });
     });
   });
 }
+
 getLeaderboard();
